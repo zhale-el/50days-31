2e23c9a73ad5cc54af9c96e64e0ef4dda16a8229
zhaleh <zhalehelmi@gmail.com>
2023-12-02 12:36:51 +0330

    add 4 functions

---
+++ script.js
@@ -0,0 +1,24 @@
+const randomFunc = {
+  lower: getRandomLower,
+  upper: getRandomUpper,
+  number: getRandomNumber,
+  Symbol: getRandomSymbol,
+};
+
+function getRandomLower() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 97);
+}
+
+function getRandomUpper() {
+  return String.fromCharCode(Math.floor(Math.random() * 26) + 65);
+}
+
+function getRandomNumber() {
+  return String.fromCharCode(Math.floor(Math.random() * 10) + 48);
+}
+
+function getRandomSymbol() {
+  const symbols = "!@#$%^&*(){}[]=<>/,.";
+
+  return symbols[Math.floor(Math.random() * symbols.length)];
+}
